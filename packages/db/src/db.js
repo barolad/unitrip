d240913cadc8121a9a1073db26dfdfb70dd66b57
barolad <artm.machulskiyy@gmail.com>
2025-05-29 18:14:22 +0300

    Удалены неиспользуемые файлы и схемы базы данных, обновлены конфигурации TypeScript и package.json. Упрощены скрипты и добавлено новое поле outDir в tsconfig.json.

--- packages/db/src/db.js
+++
@@ -1,7 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import * as schema from "./schema";
-const connectionString = String(process.env.DATABASE_URL);
-const client = postgres(connectionString, { prepare: false });
-export const db = drizzle(client, { schema });
-//# sourceMappingURL=db.js.map
--- packages/db/src/schema/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from "./users";
-export * from "./communities";
-export * from "./events";
-export * from "./categories";
